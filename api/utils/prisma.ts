faa93ab0fb842e8d03cfa0a908011b32c39771cc
Suleman Bababudan <bababudansuleman@gmail.com>
2026-08-19 14:41:41 +0100

    Add Prisma client utility

---
+++ api/utils/prisma.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "@prisma/client";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not configured");
+}
+
+const url = new URL(databaseUrl);
+
+const adapter = new PrismaMariaDb({
+  host: url.hostname,
+  port: Number(url.port) || 3306,
+  user: decodeURIComponent(url.username),
+  password: decodeURIComponent(url.password),
+  database: url.pathname.replace(/^\//, ""),
+  connectionLimit: 5,
+});
+
+export const prisma = new PrismaClient({ adapter });
