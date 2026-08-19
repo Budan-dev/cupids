d3bbc1d715167ac2cad8099323fa3388b24d17ab
Suleman Bababudan <bababudansuleman@gmail.com>
2026-08-19 14:36:32 +0100

    Update admin middleware

---
+++ api/router/middleware/adminMiddleware.ts
@@ -0,0 +1,35 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface JwtPayload {
+  id: string;
+  email: string;
+  role: string;
+}
+
+export function adminMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Authentication required, SignIn" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+
+    if (decoded.role !== "ADMIN" && decoded.role !== "SUPER_ADMIN") {
+      return res.status(403).json({ message: "Admins only" });
+    }
+
+    req.userId = decoded.id;
+    req.role = decoded.role;
+
+    next();
+  } catch {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+}
